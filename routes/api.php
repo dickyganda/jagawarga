8eb8ef1ec98b676c8b66cfe84af46f87b137fc23
Dicky Ganda <dickyganda@gmail.com>
2020-12-06 20:40:41 +0700

    update terbaru

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('login','LoginController@login');
+
+Route::get('tambahbantuan','DatabantuanController@viewtambahbantuan');
+Route::post('tambahbantuan','DatabantuanController@tambahwarga');
