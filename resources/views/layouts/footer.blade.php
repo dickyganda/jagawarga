4ee509ac92adab21c04112c349d2ae2ccb35c6d3
Dicky Ganda <dickyganda@gmail.com>
2021-06-16 19:39:14 +0700

    update data transaksi

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="">Muhamad Nauval Azhar</a>
+        Copyright &copy; <?php echo date('Y');?> <div class="bullet"></div> Made With <span color: #FF0000>&#9829</span> By <a href="">Dicky Ganda Prayogi</a>
     </div>
     <div class="footer-right">
-        2.3.0
+     
     </div>
 </footer>
